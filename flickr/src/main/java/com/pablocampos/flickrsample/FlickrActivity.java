c6de33191e6d879d6bfeb26527a4e6c62d4ccfff
Pablo Campos <pablo.campos@malauzai.com>
2018-11-06 11:21:58 -0600

    Initial commit.

---
+++ flickr/src/main/java/com/pablocampos/flickrsample/FlickrActivity.java
@@ -0,0 +1,15 @@
+package com.pablocampos.flickrsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlickrActivity extends AppCompatActivity {
+
+/* -------------------------------------- Lifecycle Methods -------------------------------------- */
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_flickr);
+	}
+/* ------------------------------------ End Lifecycle Methods ------------------------------------ */
+}
