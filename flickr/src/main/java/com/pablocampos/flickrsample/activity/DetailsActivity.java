6e27d4c18def9d3cd7d19608daa2b8ed3e85edbe
Pablo Campos <pablo.campos@malauzai.com>
2018-11-16 14:11:08 -0600

    Update: adding a palatte to modify the look and feel of the details activity.

--- flickr/src/main/java/com/pablocampos/flickrsample/activity/DetailsActivity.java
+++ flickr/src/main/java/com/pablocampos/flickrsample/activity/DetailsActivity.java
@@ -1,11 +1,22 @@
 package com.pablocampos.flickrsample.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.pablocampos.flickrsample.R;
 import com.pablocampos.flickrsample.model.FlickrFeed;
 
@@ -20,7 +31,10 @@ public class DetailsActivity extends AppCompatActivity {
 
 	public final static String FLICKR_FEED = "extra.flicker.FEED";
 
-
+	private TextView feedAuthor;
+	private TextView feedDateTaken;
+	private TextView feedDatePublished;
+	private TextView feedTags;
 
 
 
@@ -32,23 +46,65 @@ public class DetailsActivity extends AppCompatActivity {
 
 		FlickrFeed feed = (FlickrFeed) getIntent().getSerializableExtra(FLICKR_FEED);
 
-		ImageView feedImage = findViewById(R.id.feed_image);
-		Glide.with(this).load(feed.getMedia().getM()).into(feedImage);
+		final ImageView feedImage = findViewById(R.id.feed_image);
+		Glide.with(this)
+				.asBitmap()
+				.load(feed.getMedia().getM())
+				.addListener(new RequestListener<Bitmap>() {
+					@Override
+					public boolean onLoadFailed (@Nullable final GlideException e, final Object model, final Target<Bitmap> target, final boolean isFirstResource) {
+						return false;
+					}
+
+
+
+					@Override
+					public boolean onResourceReady (final Bitmap resource, final Object model, final Target<Bitmap> target, final DataSource dataSource, final boolean isFirstResource) {
+
+						updateColors(createPaletteSync(resource));
+						return false;
+					}
+				})
+		.into(feedImage);
 
 		TextView feedTitle = findViewById(R.id.feed_title);
 		feedTitle.setText(WordUtils.capitalizeFully(feed.getTitle()));
 
-		TextView feedAuthor = findViewById(R.id.feed_author);
+		feedAuthor = findViewById(R.id.feed_author);
 		feedAuthor.setText(String.format(getResources().getString(R.string.author_label), WordUtils.capitalizeFully(feed.getAuthor())));
 
-		TextView feedDateTaken = findViewById(R.id.feed_date_taken);
+		feedDateTaken = findViewById(R.id.feed_date_taken);
 		feedDateTaken.setText(String.format(getResources().getString(R.string.date_taken_label), feed.getDateTaken()));
 
-		TextView feedDatePublished = findViewById(R.id.feed_date_published);
+		feedDatePublished = findViewById(R.id.feed_date_published);
 		feedDatePublished.setText(String.format(getResources().getString(R.string.date_published_label), feed.getPublished()));
 
-		TextView feedTags = findViewById(R.id.feed_tags);
+		feedTags = findViewById(R.id.feed_tags);
 		feedTags.setText(String.format(getResources().getString(R.string.tags_label), feed.getTags()));
+	}
+
+
+
+	// Generate palette synchronously and return it
+	private Palette createPaletteSync(Bitmap bitmap) {
+		Palette p = Palette.from(bitmap).generate();
+		return p;
+	}
+
+
+
+	private void updateColors (Palette palette){
+		feedAuthor.setTextColor(palette.getVibrantColor(Color.WHITE));
+		feedDateTaken.setTextColor(palette.getVibrantColor(Color.WHITE));
+		feedDatePublished.setTextColor(palette.getVibrantColor(Color.WHITE));
+		feedTags.setTextColor(palette.getVibrantColor(Color.WHITE));
+
+		getSupportActionBar().setBackgroundDrawable(new ColorDrawable(palette.getLightMutedColor(Color.WHITE)));
+
+		Window window = getWindow();
+		window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+		window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+		window.setStatusBarColor(palette.getMutedColor(Color.WHITE));
 	}
 
 }
